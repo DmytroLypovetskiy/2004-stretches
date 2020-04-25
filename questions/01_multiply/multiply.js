cc28155a787f579fbc852fada97c6e0a0bbda5a4
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-04-25 09:30:49 -0400

    Added multiply function

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,13 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+  if (num1 === undefined || num2 === undefined) throw new Error('Values should not be empty');
 
-module.exports = { multiply };
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') throw new Error('Please provide valid numbers');
+
+  return num1 * num2;
+};
+
+module.exports = {
+  multiply
+};
--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -1,23 +1,25 @@
-const { multiply } = require('./multiply');
+const {
+  multiply
+} = require('./multiply');
 
 describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply()).toThrow('Values should not be empty');
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('hello', 'world')).toThrow('Please provide valid numbers');
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(2, 6)).toBe(12);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(0, 5)).toBe(0);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(2, -3)).toBe(-6);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
