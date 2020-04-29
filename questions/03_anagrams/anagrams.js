aee0929a224e4e43c7a9145e32610f998e60d2c7
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-04-29 18:58:49 -0400

    code cleanup

--- questions/03_anagrams/anagrams.js
+++ questions/03_anagrams/anagrams.js
@@ -2,7 +2,7 @@
 // Return a boolean
 
 const checkAnagrams = (str1, str2) => {
-  return str1.split('').sort().join(',') === str2.split('').sort().join(',');
+  return str1.split('').sort().join() === str2.split('').sort().join();
 };
 
 module.exports = {
