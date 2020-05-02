1d6131496f3b2fcb9ba1e9a2e7bcb1539059dbd4
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-02 09:15:10 -0400

    Solved palindrome

--- questions/04_palindrome/palindrome.js
+++ questions/04_palindrome/palindrome.js
@@ -1,8 +1,18 @@
 /* Given a string, determine if it is a palindrome */
 /* A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam, or racecar */
 
-const validPalindrome = () => {
-  //YOUR CODE
+const validPalindrome = (word) => {
+  const strWord = ('' + word).replace(/[^0-9a-z]/gi, '');
+  const half = Math.floor(strWord.length / 2);
+  let p2 = strWord.length - 1;
+
+  for (let p1 = 0; p1 <= half; p1++) {
+    if (strWord[p1].toLowerCase() !== strWord[p2].toLowerCase()) return false;
+    p2--;
+  }
+  return true;
 };
 
-module.exports = { validPalindrome };
+module.exports = {
+  validPalindrome
+};
