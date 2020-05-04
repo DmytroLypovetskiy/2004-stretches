84bd0a27afa672e93ed57d02a7e6731544a997ef
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-04 18:58:58 -0400

    Almost done ((

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,26 @@
-const lockBox = () => {
-  // write code here
+const lockBox = (code, msg) => {
+  console.log(typeof msg);
+  if (isNaN(code) || typeof msg !== 'string') throw new Error('wrong types');
+
+  return {
+    modCode(code, newCode) {
+      return code = newCode;
+    },
+    modMessage(msg, newMsg) {
+      return msg = newMsg;
+    },
+    revealMessage(tryCode) {
+      if (tryCode !== code) {
+        throw 'incorrect code';
+      } else {
+        return msg;
+      }
+    }
+  }
+
+
 };
 
-module.exports = { lockBox };
+module.exports = {
+  lockBox
+};
