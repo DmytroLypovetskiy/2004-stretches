429bd25e2f29450dee385412833eaa7e6e927181
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-08 16:56:49 -0400

    Finished  main testcases

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -4,6 +4,32 @@
 
 class DiceRoller {
   //YOUR CODE
+  constructor(sides, numDice) {
+    if (sides < 1 || numDice < 1 || isNaN(sides) || isNaN(numDice)) throw new Error('wrong input');
+    this._sides = sides;
+    this._numDice = numDice;
+    this._history = [];
+  }
+
+
+  set history(roll) {
+    if (roll.length) {
+      this._history.push(roll)
+    }
+  }
+
+  get history() {
+    return this._history;
+  }
+
+  roll() {
+    const roll = new Array(this._numDice).fill('').map(val => val = Math.floor(Math.random() * 6) + 1);
+    this.history = roll;
+
+    return roll;
+  }
 }
 
-module.exports = { DiceRoller };
+module.exports = {
+  DiceRoller
+};
