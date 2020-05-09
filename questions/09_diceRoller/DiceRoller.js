3e44bed0f19ed3fb82937f91529ef399316cf778
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-09 08:55:47 -0400

    UPD to private

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -8,26 +8,35 @@ class DiceRoller {
     if (sides < 1 || numDice < 1 || isNaN(sides) || isNaN(numDice)) throw new Error('wrong input');
     this._sides = sides;
     this._numDice = numDice;
-    this._history = [];
+    this._history = []
+
   }
-
-
-  set history(roll) {
-    if (roll.length) {
-      this._history.push(roll)
-    }
+  set sides(sides) {
+    this._sides = sides;
   }
-
-  get history() {
-    return this._history;
+  getSides(sides) {
+    return sides;
   }
-
+  set numDice(numDice) {
+    this._numDice = numDice;
+  }
   roll() {
-    const roll = new Array(this._numDice).fill('').map(val => val = Math.floor(Math.random() * 6) + 1);
+    const dices = this._numDice;
+    const roll = new Array(dices).fill('').map(dice => dice = Math.floor(Math.random() * 6) + 1);
+
     this.history = roll;
 
     return roll;
   }
+  set history(roll) {
+    if (roll.length) {
+      this._history.push(roll);
+    }
+  }
+  get history() {
+    return this._history;
+  }
+
 }
 
 module.exports = {
