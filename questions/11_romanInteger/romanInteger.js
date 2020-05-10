2fba5d135c2e1bb780b5d92a83d95fde3689b3bf
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-10 16:40:52 -0400

    Solved

--- questions/11_romanInteger/romanInteger.js
+++ questions/11_romanInteger/romanInteger.js
@@ -22,8 +22,62 @@ examples:
 1994 // MCMXCIV
 */
 
-const romanInteger = () => {
-  //YOUR CODE HERE
+const romanInteger = (num) => {
+  if (num < 1 || num > 3999) throw new Error('Invalid number')
+
+  const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+  const convertMap = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XV: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  let result = '';
+  /*
+  for (let key in convertMap) {
+    console.log(key)
+    while (convertMap[key] <= num) {
+      result += key;
+      num -= convertMap[key]
+    }
+  }
+  */
+
+  for (let i = 0; i < decimal.length; i++) {
+    while (decimal[i] <= num) {
+      result += roman[i];
+      num -= decimal[i];
+    }
+  }
+
+  return result;
 };
 
-module.exports = { romanInteger };
+module.exports = {
+  romanInteger
+};
