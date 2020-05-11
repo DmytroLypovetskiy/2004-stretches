15507af36e19974db739c5b2ae31eb47d3c145f0
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-11 18:55:26 -0400

    fizbuz

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,23 @@
-const fizzBuzz = () => {
-  //YOUR CODE HERE
+const fizzBuzz = (num) => {
+  const res = [];
+
+  if (!Number.isNaN(num)) {
+    throw new Error('wrong input');
+  }
+  if (num < 1) {
+    throw new Error('wrong input');
+  }
+
+  for (let i = 1; i < num; i++) {
+    if (i % 3 === 0) res.push('FIZZ');
+    if (i % 5 === 0) res.push('BUZZ')
+    if (i % 3 !== 0 || i % 5 == 0) res.push(i);
+  }
+
+  console.log(res, 'res');
+
+  return res;
 };
-module.exports = { fizzBuzz };
+module.exports = {
+  fizzBuzz
+};
