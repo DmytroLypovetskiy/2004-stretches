00262212a586e43e05927404b433afe2e2e2816d
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-12 12:29:20 -0400

    solved

--- questions/13_comparator/comparator.js
+++ questions/13_comparator/comparator.js
@@ -1,5 +1,33 @@
 //write a func that takes two arg... selectKey and obj
 
-const comparator = () => {};
+const comparator = (key, obj) => {
+  const searchObj = {
+    [key]: {}
+  };
+  const values = [];
 
-module.exports = { comparator };
+  obj.forEach(o => {
+    if (o.hasOwnProperty(key)) {
+      searchObj[key][o[key]] = o;
+      values.push(o[key]);
+    }
+  });
+
+  if (Object.values(searchObj[key]).length === 0) {
+    return 'incorrect key';
+  }
+
+  return values.sort((a, b) => {
+    if (a.length !== undefined && b.length !== undefined) {
+      return a.split(' ')[1] > b.split(' ')[1]
+    }
+    return a > b;
+  }).map(val => {
+    return searchObj[key][val];
+  });
+
+};
+
+module.exports = {
+  comparator
+};
