155aad621a7cb41022d4fd3096830febe129d868
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-15 11:13:52 -0400

    Code cleanup

--- questions/15_mockingbird/mockingBird.js
+++ questions/15_mockingbird/mockingBird.js
@@ -1,12 +1,10 @@
 // repeater runs a function n times and returns an array of its outputs
 const repeater = (func, n) => {
-  const res = new Array(n)
+  return new Array(n)
     .fill('')
     .map(item => {
       return func();
     });
-
-  return res;
 };
 
 // see test specs to complete this stretch
