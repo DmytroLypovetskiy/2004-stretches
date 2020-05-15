662987e9099ab5f4194fcd0049bbfe8aff523b1b
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-15 11:47:11 -0400

    floydsTriangle

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,28 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+  if (n === '' || n === undefined) return '';
+  if (n === 1) return `${n}`;
 
-module.exports = { floydsTriangle };
+  let res = '';
+  let counter = 1;
+
+  for (let i = 0; i < n; i++) {
+
+    for (let j = counter; j <= counter + i; j++) {
+      res += j;
+    }
+
+    res += '\n';
+
+    counter++;
+    console.log(res);
+  }
+
+  return res;
+};
+
+module.exports = {
+  floydsTriangle
+};
