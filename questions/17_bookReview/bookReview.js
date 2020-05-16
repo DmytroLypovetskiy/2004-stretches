fde664d3e163b9ca7cf0fa8e912481a12222d385
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-16 09:13:50 -0400

    Solved

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,23 @@
-class BookReview {}
-module.exports = { BookReview };
+class BookReview {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.notes = [];
+    this.rating = null;
+  }
+  addNotes(pageNum, note) {
+    if (this.notes[pageNum] === undefined) {
+      this.notes[pageNum] = note;
+    } else {
+      this.notes[pageNum] += ` ${note}`;
+    }
+
+  }
+  addRating(rating) {
+    if (rating < 1 || rating > 5) throw new Error('wrong rating');
+    this.rating = rating;
+  }
+}
+module.exports = {
+  BookReview
+};
