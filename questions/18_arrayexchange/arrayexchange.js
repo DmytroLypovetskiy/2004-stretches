617f86f10ee84dc4c399a6cb996ccd3dea0fcff3
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-17 14:14:44 -0400

    Solved

--- questions/18_arrayexchange/arrayexchange.js
+++ questions/18_arrayexchange/arrayexchange.js
@@ -1,6 +1,16 @@
 //arrayexchange should exchange the elements of two arrays in-place in a way that their new content is also reversed.
 //restricted use: reverse()
 
-const arrayexchange = () => {};
+const arrayexchange = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) throw new Error('Both arguments should be arrays');
 
-module.exports = { arrayexchange };
+  const tmp = [...arr1.reverse()];
+  arr1.length = 0;
+  arr1.push(...arr2.reverse());
+  arr2.length = 0;
+  arr2.push(...tmp);
+};
+
+module.exports = {
+  arrayexchange
+};
