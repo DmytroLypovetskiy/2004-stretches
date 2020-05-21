1e2a32cb0debf19947dcf860f0816969ebd78fdd
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-21 17:36:20 -0400

    UPD

--- questions/22_moveZeroes/moveZeroes.js
+++ questions/22_moveZeroes/moveZeroes.js
@@ -1,5 +1,17 @@
 const moveZeroes = (arr) => {
-  //YOUR CODE HERE
+  const empties = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      empties.push(arr[i]);
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  arr.push(...empties);
+  return arr;
 };
 
-module.exports = { moveZeroes };
+module.exports = {
+  moveZeroes
+};
