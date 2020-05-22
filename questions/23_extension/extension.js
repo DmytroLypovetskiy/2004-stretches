258ac7ae3941a832b425aa804e1c887ecf925125
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-22 11:07:32 -0400

    Solved

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,19 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (cb) => {
+  if (typeof cb !== 'function') throw new Error('Callback is not a functuin');
 
-module.exports = { extensions };
+  return (val1) => {
+    if (isNaN(val1)) throw new Error('Is not a number');
+
+    return (val2) => {
+      if (isNaN(val2)) throw new Error('Is not a number');
+
+      return cb(val1, val2);
+    };
+  }
+};
+
+module.exports = {
+  extensions
+};
