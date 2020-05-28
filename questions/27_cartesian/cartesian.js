26a28eb0f84ea47b245c9989fa52a1003e839dbb
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-28 10:36:24 -0400

    Solved

--- questions/27_cartesian/cartesian.js
+++ questions/27_cartesian/cartesian.js
@@ -1,5 +1,26 @@
-const cartesian = () => {
-  //YOUR CODE GOES HERE
+const cartesian = (dir) => {
+  if (!Array.isArray(dir)) throw new Error('Is not array');
+
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < dir.length; i++) {
+    if (dir[i] === 'n') x++;
+    if (dir[i] === 's') x--;
+    if (dir[i] === 'w') y++;
+    if (dir[i] === 'e') y--;
+  }
+
+  if (x === 0 && y === 0) return "These directions don't go anywhere!";
+
+  return {
+    n: x > 0 ? x : 0,
+    s: x < 0 ? x : 0,
+    w: y > 0 ? y : 0,
+    e: y < 0 ? y : 0,
+  }
 };
 
-module.exports = { cartesian };
+module.exports = {
+  cartesian
+};
