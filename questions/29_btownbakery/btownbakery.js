0d286b1661a785b41d3c9edccaacb7ef225545b6
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-05-30 09:15:14 -0400

    Solved

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,21 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
-  //enter code here
+const btownbake = (recipe, supplies) => {
+  if (typeof recipe !== 'object' || typeof supplies !== 'object') throw new Error('Both arguments should be Objects');
+  const supCounter = [];
+
+  for (let key in recipe) {
+    if (supplies.hasOwnProperty(key) || supplies[key] >= recipe[key]) {
+      cakes++;
+      supCounter.push(Math.floor(supplies[key] / recipe[key]))
+    } else {
+      return 0;
+    }
+  }
+
+  return Math.min(...supCounter)
 };
 
-module.exports = { btownbake };
+module.exports = {
+  btownbake
+};
