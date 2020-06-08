e49f63e84b2203514ad4acac7535f105edfc6eba
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-08 18:56:51 -0400

    Solved

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,10 @@
-const decypher = () => {
-  //YOUR CODE HERE
+const decypher = (str) => {
+  return str
+    .split(' ')
+    .map(word => {
+      return String.fromCharCode(parseInt(word)) + word[word.length - 1] + word.slice(2, -1) + word[1];
+    }).join(' ');
 };
-module.exports = { decypher };
+module.exports = {
+  decypher
+};
