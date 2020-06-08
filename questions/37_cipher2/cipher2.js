ee58e1dc4f2c929aa6bfb60ba50be3b18562390d
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-08 19:10:57 -0400

    UPD

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -2,7 +2,10 @@ const decypher = (str) => {
   return str
     .split(' ')
     .map(word => {
-      return String.fromCharCode(parseInt(word)) + word[word.length - 1] + word.slice(2, -1) + word[1];
+      console.log(word);
+      console.log(String.fromCharCode(parseInt(word)), word[word.length - 1], word.slice(4, -1), word[3]);
+
+      return String.fromCharCode(parseInt(word)) + word[word.length - 1] + word.slice(4, -1) + word[3];
     }).join(' ');
 };
 module.exports = {
