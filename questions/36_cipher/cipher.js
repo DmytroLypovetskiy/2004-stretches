483b683417009d69d95b05e9d3773b05b9cc3bec
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-07 21:56:59 -0400

    Solved

--- questions/36_cipher/cipher.js
+++ questions/36_cipher/cipher.js
@@ -1,5 +1,11 @@
-const encrypt = () => {
-  //YOUR CODE HERE
+const encrypt = (str) => {
+  return str
+    .split(' ')
+    .map(word => {
+      return word.charCodeAt(0) + word[word.length - 1] + word.slice(2, -1) + word[1];
+    }).join(' ')
 };
 
-module.exports = { encrypt };
+module.exports = {
+  encrypt
+};
