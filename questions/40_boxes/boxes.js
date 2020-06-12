5ec45147988fc42d394780e2c9ae235d3ebb9c8c
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-11 20:33:23 -0400

    solved

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,16 @@
-//YOUR CODE HERE
+class Box {
+  constructor(color = '', capacity = 0, contents = []) {
+    this.color = color;
+    this.capacity = capacity;
+    this.contents = contents;
+  }
+  pack(color = '', capacity = 0, contents = []) {
+    const box = new Box(color, capacity, contents);
+    this.contents.push(box)
+  }
 
-module.exports = { Box };
+}
+
+module.exports = {
+  Box
+};
