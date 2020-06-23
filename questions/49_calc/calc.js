f5b1c34d537b75f08041535cc155736d81c4b1c1
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-23 12:25:14 -0400

    Solved

--- questions/49_calc/calc.js
+++ questions/49_calc/calc.js
@@ -1,7 +1,24 @@
 // - Write a chainable Calculator that can add, subtract,and multiply
 
 class chainCalc {
-  //write code here
+  constructor(num) {
+    if (isNaN(num)) throw new Error('not a number');
+    this.result = num;
+  }
+  add(num) {
+    this.result += num;
+    return this;
+  }
+  sub(num) {
+    this.result -= num;
+    return this;
+  }
+  mul(num) {
+    this.result *= num;
+    return this;
+  }
 }
 
-module.exports = { chainCalc };
+module.exports = {
+  chainCalc
+};
