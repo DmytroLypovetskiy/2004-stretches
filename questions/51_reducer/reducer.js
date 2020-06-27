8f421329f6bd4d2fef6e9737d857707978694da3
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-06-27 09:21:38 -0400

    2 specs

--- questions/51_reducer/reducer.js
+++ questions/51_reducer/reducer.js
@@ -8,20 +8,39 @@ const types = {
   COMPLETE_TODO: 'COMPLETE_TODO',
 };
 
-const initialState = [
-  {
-    item: 'Use Redux',
-    completed: false,
-    id: 0,
-  },
-];
+const initialState = [{
+  item: 'Use Redux',
+  completed: false,
+  id: 0,
+}, ];
 
 const reducer = (state = initialState, action) => {
   //ADD YOUR CODE HERE
+  //console.log(state);
+  //console.log(action, 'action');
   switch (action.type) {
+    case 'ADD_TODO':
+      console.log(action, 'action');
+      return [{
+          text: action.text,
+          completed: false,
+          id: state.length
+        },
+        ...state,
+      ]
+    case 'DELETE_TODO':
+      console.log(action, 'action');
+      return [
+        ...state,
+        action.text
+      ]
     default:
-      return;
+      return state
   }
 };
 
-module.exports = { types, initialState, reducer };
+module.exports = {
+  types,
+  initialState,
+  reducer
+};
